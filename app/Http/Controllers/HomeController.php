aecc540b0e23d62bc195e6a8948170ec036ed37f
Lyndon.lin <wuhulinyi@sina.com>
2017-06-13 10:38:06 +0800

    添加菜单选择图标时，改为可搜索

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
+use Auth;
 use Cookie;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 
 class HomeController extends Controller
 {
@@ -14,7 +17,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+//        $this->middleware('auth');
     }
 
     /**
@@ -24,6 +27,27 @@ class HomeController extends Controller
      */
     public function index(Request $request)
     {
-        return view('home');
+//        for ($i = 0; $i < 10; $i++) {
+//            echo Redis::spop('prize');
+//        }
+//        ini_set('max_execution_time', 300);
+//        for ($i = 0; $i < 50000; $i++) {
+//            Redis::sadd('prize',uniqid());
+//        }
+        echo Redis::scard('prize');
+        echo "<br>";
+        echo Redis::scard('log');
+    }
+
+    public function test()
+    {
+        $u = User::find(1);
+        Auth::login($u);
+        $prize = Redis::spop('prize');
+        if($prize){
+            $d = ['user'=>$u->name,'id'=>$prize];
+            Redis::sadd('log',json_encode($d));
+        }
+        echo Redis::scard('log');
     }
 }
